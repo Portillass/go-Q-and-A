1e8f22f46a773b4e522157d0c838fb5b7371d060
Jarell E. Portillas <2201107699@student.buksu.edu.ph>
2025-10-09 20:56:20 +0800

    1st Q&A

---
+++ main.go
@@ -0,0 +1,9 @@
+/*Q1: Write a simple “Hello World” program in Go.*/
+
+package main // Difines the main package - entry poiny of evert Go Program
+
+import "fmt" // Import the fmt package for formatted I/O
+
+func main() {   // main() in the starting point of the program
+	fmt.Println("Hello, World!") // Prints "Hello, World!" to the console
+}
