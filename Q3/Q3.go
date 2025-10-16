bdd813157a1a6939416f34a43cfc78c09fe91fbb
Jarell E. Portillas <2201107699@student.buksu.edu.ph>
2025-10-16 21:09:19 +0800

    lol

--- Q3/Q3.go
+++ Q3/Q3.go
@@ -1,9 +1,11 @@
 // Q3: Write Go porgram that subtracts two numbers.
 
 package Q3 // import package
- import "fmt"
+ import "fmt" 
 
- func Subtract(){
+
+
+ func Subtract(){ // fucntion
 	a, b := 10, 4
 	diff := a -b
 	fmt.Println("Difference:",diff)
